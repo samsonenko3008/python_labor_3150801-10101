8a9a584d416468b7cdf710fe170e550281db2d92
samsonenko3008 <149273438+samsonenko3008@users.noreply.github.com>
2026-05-07 15:39:07 +0300

    Добавление системы скидок, вычисление итоговой суммы

--- task_05_discount_calculator.py
+++ task_05_discount_calculator.py
@@ -3,3 +3,17 @@
 # Шаг 1: Задаем цену за единицу и количество товара
 unit_price = 1200.0  # Цена за единицу
 quantity = 5         # Количество
+
+# Шаг 2: Расчет общей стоимости и применение скидок
+total_raw = unit_price * quantity
+
+if total_raw < 1000:
+    discount_percent = 0
+elif 1000 <= total_raw <= 5000:
+    discount_percent = 5
+else:
+    discount_percent = 10
+
+# Вычисление итоговой суммы
+discount_amount = (total_raw * discount_percent) / 100
+final_price = total_raw - discount_amount
