2f32fcbcdc087a7c2e72623ca60b23c90a723042
samsonenko3008 <149273438+samsonenko3008@users.noreply.github.com>
2026-05-07 15:26:20 +0300

    Добавление логики определения дня недели

--- task_04_foundation_load.py
+++ task_04_foundation_load.py
@@ -3,3 +3,12 @@
 # Задаем номер дня недели от 1 до 7 (1 — Понедельник, 7 — Воскресенье)
 day_number = 3  # Любое число от 1 до 7
 
+# Определение дня недели
+if day_number == 1: day_name = "Понедельник"
+elif day_number == 2: day_name = "Вторник"
+elif day_number == 3: day_name = "Среда"
+elif day_number == 4: day_name = "Четверг"
+elif day_number == 5: day_name = "Пятница"
+elif day_number == 6: day_name = "Суббота"
+elif day_number == 7: day_name = "Воскресенье"
+else: day_name = "Неизвестный день"
